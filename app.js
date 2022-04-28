f330dd4b267f691e589fec8bdc6d027f04e7da59
Casey <89794911+cmurphy20-CVTC@users.noreply.github.com>
2022-04-27 20:29:19 -0500

    Added Level 3 - Hashing Passwords

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const encrypt = require('mongoose-encryption');
+const md5 = require("md5");
 
 const app = express();
 
@@ -20,8 +20,6 @@ const userSchema = new mongoose.Schema ({
   password: String
 
 });
-
-userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ["password"] });
 
 
 
@@ -43,7 +41,7 @@ app.post("/register", function(req, res){
 
   const newUser = new User({
     email: req.body.username,
-    password: req.body.password
+    password: md5(req.body.password)
   });
 
   newUser.save(function(err){
