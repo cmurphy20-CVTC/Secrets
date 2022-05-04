78b824a9a9681e5c9fbc73586cd2cce08a361960
Casey <89794911+cmurphy20-CVTC@users.noreply.github.com>
2022-05-03 20:48:32 -0500

    adding in passport

--- app.js
+++ app.js
@@ -4,14 +4,23 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const bcrypt = require("bcrypt");
-const saltRounds = 10;
+const session = require('express-session');
+const passport = require("passport");
+const passportLocalMongoose = require("passport-local-mongoose");
+
 
 const app = express();
 
 app.use(express.static("public"));
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(session({
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: true,
+  cookie: { secure: true }
+}));
+
 
 mongoose.connect("mongodb://localhost:27017/userDB", {useNewUrlParser: true});
 
@@ -21,8 +30,6 @@ const userSchema = new mongoose.Schema ({
   password: String
 
 });
-
-
 
 User = mongoose.model("User", userSchema);
 
@@ -40,44 +47,12 @@ app.get("/register", function(req, res){
 
 app.post("/register", function(req, res){
 
-  bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
-
-    const newUser = new User({
-      email: req.body.username,
-      password: hash
-    });
   
-    newUser.save(function(err){
-      if(err) {
-        console.log(err);
-      } else {
-        res.render("secrets");
-      };
-    });
 });
 
+app.post("/login", function(req, res){
 
 
-});
-
-app.post("/login", function(req, res) {
-  const username = req.body.username;
-  const password = req.body.password;
-
-  User.findOne({email: username}, function(err, foundUser){
-    if (err) {
-      console.log(err);
-    } else {
-      if (foundUser) {
-        bcrypt.compare(password, foundUser.password, function(err, result) {
-          if (result === true) {
-            res.render("secrets");
-          } 
-        });
-          
-      };
-      };
-  });
 });
 
 app.listen(3000, function(){
